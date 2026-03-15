91de71a27b8648ef945d76b750a432830b4c79e9
mauricioandrade <mauricio_andrade@outlook.com>
2026-03-14 22:26:51 -0300

    first commit

---
+++ backend/progressor/src/test/java/com/mauricioandrade/progressor/ProgressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricioandrade.progressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
